ffb3325a64ca83ff4123ffc999c92640f45c1b22
surajtib <suraj.rauda@trooinbound.com>
2024-01-02 18:44:23 +0530

    done

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,4 +1,3 @@
-// Todo.js
 import React, { useState, useEffect } from 'react';
 import { InputGroup, FormControl, Button, ListGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,41 +6,94 @@ import axios from 'axios';
 const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const [offlineTodo, setOfflineTodo] = useState('');
+
+  console.log("offlineTodo", offlineTodo)
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/todos');
+      setTodos(response.data);
+
+      // Check if there are locally stored todos
+      const localTodos = JSON.parse(localStorage.getItem('localTodos')) || [];
+
+      if (localTodos.length > 0) {
+        // Send locally stored todos to the server
+
+console.log("localtodo", ...localTodos);
+        await axios.post('http://localhost:3001/todos', ...localTodos);
+
+        // Clear local storage after sending todos to the server
+        localStorage.removeItem('localTodos');
+      }
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const response = await axios.get('/.netlify/functions/server/todos');
-        setTodos(response.data);
-      } catch (error) {
-        console.error('Error fetching todos:', error);
-      }
-    };
-
     fetchTodos();
   }, []);
 
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      const isConnectedToNetwork = navigator.onLine;
+      console.log("isConnectedToNetwork", isConnectedToNetwork);
+  
+      if (isConnectedToNetwork && offlineTodo?.length > 0) {
+        for (const data of offlineTodo) {
+          try {
+            const response = await axios.post('http://localhost:3001/todos', data);
+            console.log("Success:", response);
+          } catch (error) {
+            console.error('Error posting todo:', error);
+          }
+        }
+  
+        setOfflineTodo([]);
+      }
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, [offlineTodo]);
+  
+
   const addTodo = async () => {
     const isConnectedToNetwork = navigator.onLine;
-
+    // const isConnectedToNetwork = true;
+  
     if (newTodo.trim() !== '') {
       try {
         if (isConnectedToNetwork) {
-          const response = await axios.post('/.netlify/functions/server/todos', {
+          // If online, send the new todo to the server
+          const response = await axios.post('http://localhost:3001/todos', {
             text: newTodo,
             completed: false,
           });
-
+  
           setTodos([...todos, response.data]);
           setNewTodo('');
-        } else {
-          // Save to local storage if not connected to network
+  
+          // Check if there are locally stored todos
           const localTodos = JSON.parse(localStorage.getItem('localTodos')) || [];
-          const todoToAdd = { id: Date.now(), text: newTodo, completed: false };
-          localTodos.push(todoToAdd);
-          localStorage.setItem('localTodos', JSON.stringify(localTodos));
+  
+          if (localTodos.length > 0) {
+            // Send locally stored todos to the server
+            console.log("localTodos", localTodos)
 
-          setTodos([...todos, todoToAdd]);
+            await axios.post('http://localhost:3001/todos', localTodos);
+  
+            // Clear local storage after sending todos to the server
+            localStorage.removeItem('localTodos');
+          }
+        } else {
+          // If offline, save the new todo to local storage
+          const todoToAdd = { id: Date.now(), text: newTodo, completed: false };
+  
+          // Concatenate the local todos directly to the current todos state
+          setTodos((prevTodos) => [...prevTodos, todoToAdd]);
+          setOfflineTodo((prevTodos) => [...prevTodos, todoToAdd]);
+  
           setNewTodo('');
         }
       } catch (error) {
@@ -49,7 +101,7 @@ const Todo = () => {
       }
     }
   };
-
+  
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       addTodo();
